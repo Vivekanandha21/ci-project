261b5102277050c6f34e5e5a95317c2c633c59a7
Vivekanandha21 <133024764+Vivekanandha21@users.noreply.github.com>
2025-03-30 00:56:37 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull vivekanandha/simple-python-flask-app:latest
+docker pull vivekanandha/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 docker pull vivekanandha/simple-python-flask-app
+docker run -d -p 5000:5000 vivekanandha/simple-python-flask-app
