c5bb593e293e17d963e4562409dfc90ec6afa3b5
Vivekanandha21 <133024764+Vivekanandha21@users.noreply.github.com>
2025-04-01 00:44:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    message = "Hope you are getting an idea on how it works!!!"
+    message = "Hope you are Learning & getting an idea on how it works!!!"
     return f"{message}<br>HELLO TO VK WORLD! FIRST CI/CD PROJECT"
 
 if __name__ == '__main__':
