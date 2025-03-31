00f0008638b8614181a6ee89dfa7bc60e925df21
Vivekanandha21 <133024764+Vivekanandha21@users.noreply.github.com>
2025-04-01 00:06:46 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull docker pull vivekanandha/cicd-python-app-deploy-project
+docker pull vivekanandha/cicd-python-app-deploy-project
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 vivekanandha/cicd-python-app-deploy-project
